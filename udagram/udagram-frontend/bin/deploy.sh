d9e4cecc6c9b2239dd0ccd0be13bb78ff48868ff
Alessandro Malhotra <alessandro244@outlook.com>
2023-05-26 00:03:10 +0100

    Updating access point

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://arn:aws:s3:::udigram/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://arn:aws:s3:::udigram/
+aws s3 cp --recursive --acl public-read ./www s3://arn:aws:s3:us-east-1:844951663216:accesspoint/udigram
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://arn:aws:s3:us-east-1:844951663216:accesspoint/udigram
